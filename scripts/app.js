a71fcc3f94c08c4aa6c2cfee0574de0db408da09
Karan Taneja <karantaneja@pursuit.org>
2019-02-02 16:57:25 -0500

    Inital build of the mocking string generator

---
+++ scripts/app.js
@@ -0,0 +1,64 @@
+
+//----FUNCTION
+
+const  mock = (string) => {
+
+    let mockString = "";
+
+    for(let i = 0; i < string.length; i++){
+
+        const randomizer = Math.round(Math.random());
+
+        if(randomizer === 1){
+            mockString += string[i].toUpperCase();
+        }
+        else{
+            mockString += string[i].toLowerCase();
+        };
+
+    };
+
+    return mockString;
+}
+
+//----QUERY SELECTORS
+
+const input = document.querySelector('.js-input');
+const output = document.querySelector('.js-output');
+const website = document.querySelector('.js-website');
+
+//----EVENT LISTENERS
+
+window.addEventListener('load', (e) => {
+    website.innerText = mock(website.innerText);
+});
+
+input.addEventListener('input', (e) => {
+
+    const inputString = input.value
+    const outputString = mock(inputString);
+    
+    output.value=outputString;
+
+})
+
+input.addEventListener('keyup', (e) => {
+
+    if(e.key === "Enter"){
+        if(parseInt(input.attributes.rows.value) < 5){
+            input.attributes.rows.value = `${parseInt(input.attributes.rows.value)+1}`
+            output.attributes.rows.value = `${parseInt(input.attributes.rows.value)+1}`
+        }
+    }
+
+});
+
+output.addEventListener('click', (e) => {
+
+    if(output.value.length > 0){
+        output.select();
+        document.execCommand("copy")
+        alert("Copied: " + output.value);
+    }
+
+})
